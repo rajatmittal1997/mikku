353595b56f625ec3a1d336ab0342d096aafa086e
Rajat Mittal <rajatmittal1997@gmail.com>
2025-08-05 19:14:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       I love  You Mikku
+      </header>
+    </div>
+  );
+}
+
+export default App;
